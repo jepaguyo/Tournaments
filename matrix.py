a3607c0db14337d5f811a435aabd08074503b4aa
Peter Kagey <peterkagey@gmail.com>
2020-03-26 14:14:24 -0700

    fix class methods with self

--- matrix.py
+++ matrix.py
@@ -1,15 +1,15 @@
 class Matrix:
-    def __init__(self,n,edges):
-        Matrix.adj = self.adjacency_matrix(n,edges)
+    def __init__(self, n, edges):
+        self.adj = self.adjacency_matrix(n, edges)
 
-    def zero_matrix(n):
+    def zero_matrix(self, n):
         m = []
         for i in range(n):
             m.append([0]*n)
         return m
 
-    def adjacency_matrix(n, edges):
-        m = zero_matrix(n)
+    def adjacency_matrix(self, n, edges):
+        m = self.zero_matrix(n)
         for (i,j) in edges:
             m[i-1][j-1] = 1
         return m
